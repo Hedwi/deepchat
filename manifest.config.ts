fd96877bb99b916e258ae1d39d1909158e60409f
solos <lxl1217@gmail.com>
2025-01-08 21:50:36 +0800

    update locales
    update locales

--- manifest.config.ts
+++ manifest.config.ts
@@ -15,7 +15,7 @@ export default defineManifest(async (env) => ({
     "DeepChat is a browser extension that allows you to use DeepSeek's LLM/ChatGPT in any website.",
   version: `${major}.${minor}.${patch}.${label}`,
   version_name: version,
-  default_locale: 'zh',
+  default_locale: 'en_US',
   action: {
     default_title: 'DeepChat - Open Sidebar',
   },
