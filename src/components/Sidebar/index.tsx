14830be72804e276313653b342c8dd5a576ca853
solos <lxl1217@gmail.com>
2025-03-03 13:53:46 +0800

    fix background color
    fix background color

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,7 +8,7 @@ function Sidebar() {
   const [settings] = useSettings()
   useThemeSync()
   return (
-    <div className="cdx-flex cdx-backdrop-blur-sm cdx-flex-col cdx-min-h-screen cdx-shadow-md cdx-border-l dark:!cdx-text-white dark:cdx-border-neutral-800 cdx-border-neutral-200 cdx-top-0 cdx-right-0 cdx-w-[600px] cdx-h-full dark:cdx-bg-neutral-800/90 cdx-bg-neutral-100/90">
+    <div className="cdx-flex cdx-backdrop-blur-sm cdx-flex-col cdx-min-h-screen cdx-shadow-md cdx-border-l dark:!cdx-text-white dark:cdx-border-neutral-800 cdx-border-neutral-200 cdx-top-0 cdx-right-0 cdx-w-[600px] cdx-h-full dark:cdx-bg-neutral-800/90 cdx-bg-neutral">
       <Header />
       {settings.chat.openAIKey && settings.chat.model ? (
         <Chat settings={settings} />
